308e0f2002dd1d6b022cf39cc92cbd0789e93cf9
HP <HP@DESKTOP-26HIJMS>
2023-10-12 17:19:34 +0530

    New framework

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,17 @@
+package BaseLayer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+	
+	public void initialization() {
+		
+		WebDriver driver =new ChromeDriver();
+		driver.get("");
+		driver.manage().window().maximize();
+		driver.manage().
+	}
+
+}
